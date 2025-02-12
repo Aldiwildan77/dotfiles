15584d3b84b9c95e91d0145a01fb2e1b338a7db4
Aldiwildan77 <aldiwild77@gmail.com>
2025-02-12 23:47:16 +0700

    feat: add go path bin

--- zsh/zshrc.d/exports.zsh
+++ zsh/zshrc.d/exports.zsh
@@ -1,6 +1,7 @@
 # Golang
 export GOPATH="$HOME/go"
 export PATH="$GOPATH/bin:$PATH"
+export PATH=$PATH:/usr/local/go/bin
 
 ## Skip protobuf conflict namespace
 export GOLANG_PROTOBUF_REGISTRATION_CONFLICT=warn
